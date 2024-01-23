0f1b57c95c96d54b5bb090f5ef73845007aa3d80
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2024-01-23 22:34:02 +0300

    add checking

--- tests/test_demoqa_form.py
+++ tests/test_demoqa_form.py
@@ -1,7 +1,6 @@
-from selene import browser, have, be
+from selene import browser, have, be, by
 from selene.core import command
 import os
-
 
 def test_demoqa_form():
     browser.open('/')
@@ -9,8 +8,13 @@ def test_demoqa_form():
     browser.element('#lastName').should(be.blank).send_keys('Ivanov')
     browser.element('#userEmail').should(be.blank).send_keys('Ivanov@test.com')
     browser.element('#gender-radio-1').double_click()
-    browser.element('#userNumber').should(be.blank).send_keys('89999876543')
-    browser.element('#dateOfBirthInput').perform(command.js.set_value('10 Jan 1980'))
+    browser.element('#userNumber').should(be.blank).send_keys('1234567890')
+    # browser.element('#dateOfBirthInput').perform(command.js.set_value('10 Jan 1980'))
+    browser.element('#dateOfBirthInput').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element(".react-datepicker__year-select").click().element(by.text("1980")).click()
+    browser.element(".react-datepicker__month-select").click().element(by.text("January")).click()
+    browser.element(".react-datepicker__day--010").click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('picture.jpg'))
     browser.element("[class='custom-control custom-checkbox custom-control-inline']").click()
     browser.element('#subjectsInput').type('Physics').press_enter()
@@ -19,17 +23,16 @@ def test_demoqa_form():
     browser.element('#react-select-4-input').type('Delhi').press_enter()
     browser.element('#submit').submit()
     browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
-    browser.element('.table').all('td').should(have.texts(
+    browser.element('.table').all('td:nth-child(2)').should(have.exact_texts(
         'Ivan Ivanov',
         'Ivanov@test.com',
         'Male',
-        '89999876543',
-        '10 Jan 1980',
+        '1234567890',
+        '10 January,1980',
         'Physics',
         'Sports',
         'picture.jpg',
         '111999, St Hall avenue 34',
-        'NCR Delhi'
-
+        'NCR Delhi',
     ))
     browser.element('#closeLargeModal').double_click()
